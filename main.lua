ceac2093da81819a0de577d806dfba8de8b3e13a
Mick McCarthy <mccarthy.mick0012@gmail.com>
2018-03-13 18:15:50 +0000

    Add ability to regenerate grid

--- main.lua
+++ main.lua
@@ -18,4 +18,11 @@ function love.keypressed(key)
     if key == 'escape' then
         love.event.quit()
     end
+    if key == 'space' then
+        _rebuildGrid()
+    end
+end
+
+function _rebuildGrid()
+    grid = Grid.create()
 end
